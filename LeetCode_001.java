7f2e127d406b4423ee7fecbed71b3c54f17e7788
biyunfei3 <biyunfei3@jd.com>
2023-05-20 17:08:28 +0800

    力扣代码上传

--- LeetCode_001.java
+++ LeetCode_001.java
@@ -8,5 +8,17 @@ public class LeetCode_001 {
 
     }
 
-
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans[] = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = map.get(target - nums[i]);
+                ans[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
 }
